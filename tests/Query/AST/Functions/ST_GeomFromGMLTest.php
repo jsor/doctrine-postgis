9b09544399a6d863c4924b83b47363e05b57ead1
Jan Sorgalla <jsorgalla@gmail.com>
2015-03-26 08:37:08 +0100

    Fix test for postgis 1.5

--- tests/Query/AST/Functions/ST_GeomFromGMLTest.php
+++ tests/Query/AST/Functions/ST_GeomFromGMLTest.php
@@ -39,6 +39,36 @@ class ST_GeomFromGMLTest extends AbstractFunctionalTestCase
 
     public function testQuery1()
     {
+        $query = $this->_getEntityManager()->createQuery('SELECT ST_AsEWKT(ST_GeomFromGML(ST_AsGML(\'POLYGON((0 0,0 1,1 1,1 0,0 0)\'))) FROM Jsor\\Doctrine\\PostGIS\\PointsEntity');
+
+        $result = $query->getSingleResult();
+
+        array_walk_recursive($result, function (&$data) {
+            if (is_resource($data)) {
+                $data = stream_get_contents($data);
+
+                if (false !== ($pos = strpos($data, 'x'))) {
+                    $data = substr($data, $pos + 1);
+                }
+            }
+
+            if (is_string($data)) {
+                $data = trim($data);
+            }
+        });
+
+        $expected = array (
+  1 => 'POLYGON((0 0,0 1,1 1,1 0,0 0)',
+);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @group postgis-2.x
+     */
+    public function testQuery2()
+    {
         $query = $this->_getEntityManager()->createQuery('SELECT ST_AsEWKT(ST_GeomFromGML(\'<gml:Polygon srsName="EPSG:4326"><gml:outerBoundaryIs><gml:LinearRing><gml:coordinates>0,0 0,1 1,1 1,0 0,0</gml:coordinates></gml:LinearRing></gml:outerBoundaryIs></gml:Polygon>\')) FROM Jsor\\Doctrine\\PostGIS\\PointsEntity');
 
         $result = $query->getSingleResult();
@@ -67,7 +97,7 @@ class ST_GeomFromGMLTest extends AbstractFunctionalTestCase
     /**
      * @group postgis-2.x
      */
-    public function testQuery2()
+    public function testQuery3()
     {
         $query = $this->_getEntityManager()->createQuery('SELECT ST_AsEWKT(ST_GeomFromGML(\'<gml:LineString><gml:coordinates>-71.16028,42.258729 -71.160837,42.259112 -71.161143,42.25932</gml:coordinates></gml:LineString>\', 4326)) FROM Jsor\\Doctrine\\PostGIS\\PointsEntity');
 
--- tools/postgis-functions/8.4-geometry-constructors.php
+++ tools/postgis-functions/8.4-geometry-constructors.php
@@ -138,6 +138,13 @@ return array(
         'tests' => array(
             'queries' => array(
                 array(
+                    'sql' => "SELECT ST_AsEWKT({function}(ST_AsGML('POLYGON((0 0,0 1,1 1,1 0,0 0)')))",
+                    'result' => array(
+                        1 => 'POLYGON((0 0,0 1,1 1,1 0,0 0)'
+                    )
+                ),
+                array(
+                    'group' => 'postgis-2.x',
                     'sql' => "SELECT ST_AsEWKT({function}('<gml:Polygon srsName=\"EPSG:4326\"><gml:outerBoundaryIs><gml:LinearRing><gml:coordinates>0,0 0,1 1,1 1,0 0,0</gml:coordinates></gml:LinearRing></gml:outerBoundaryIs></gml:Polygon>'))",
                     'result' => array(
                         1 => 'SRID=4326;POLYGON((0 0,0 1,1 1,1 0,0 0)'
