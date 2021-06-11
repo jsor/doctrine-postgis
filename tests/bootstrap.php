7e85c0cb21da6b6f10cdd4165f9b9d8f5509bdcf
Maxime Veber <nek.dev@gmail.com>
2021-06-11 18:20:06 +0200

    fix: add creation of postgis extension to boostrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,19 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Configuration;
+
 $loader = require __DIR__ . '/../vendor/autoload.php';
 
 $GLOBALS['TESTS_TEMP_DIR'] = __DIR__ . '/temp';
+
+var_dump($GLOBALS);
+$connection = DriverManager::getConnection([
+    'driver' => $GLOBALS['db_type'],
+    'user' => $GLOBALS['db_username'],
+    'password' => $GLOBALS['db_password'],
+    'host' => $GLOBALS['db_host'],
+    'dbname' => $GLOBALS['db_name'],
+    'port' => $GLOBALS['db_port'],
+], new Configuration());
+$connection->executeQuery('CREATE EXTENSION IF NOT EXISTS postgis');
