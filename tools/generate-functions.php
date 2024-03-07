4ec73b946907f4352a3d30e94deb1aecda582799
Gwendolen Lynch <gwendolen.lynch@gmail.com>
2024-03-07 17:06:15 +0100

    Switch functions from Lexer constants to TokenType enum

--- tools/generate-functions.php
+++ tools/generate-functions.php
@@ -74,7 +74,7 @@ namespace Jsor\Doctrine\PostGIS\Functions;
 
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\AST\Node;
-use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\TokenType;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\SqlWalker;
 
@@ -84,13 +84,13 @@ final class <?php echo $name; ?> extends FunctionNode
 
     public function parse(Parser $parser): void
     {
-        $parser->match(Lexer::T_IDENTIFIER);
-        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(TokenType::T_IDENTIFIER);
+        $parser->match(TokenType::T_OPEN_PARENTHESIS);
 <?php if ($totalArguments > 0) { ?>
     <?php for ($i = 0; $i < $requiredArguments; ++$i) { ?>
         <?php if ($i > 0) { ?>
 
-        $parser->match(Lexer::T_COMMA);
+        $parser->match(TokenType::T_COMMA);
         <?php } ?>
 
         $this->expressions[] = $parser->ArithmeticFactor();
@@ -101,14 +101,14 @@ final class <?php echo $name; ?> extends FunctionNode
         $lexer = $parser->getLexer();
         <?php } ?>
 
-        if ($lexer->isNextToken(Lexer::T_COMMA)) {
-            $parser->match(Lexer::T_COMMA);
+        if ($lexer->isNextToken(TokenType::T_COMMA)) {
+            $parser->match(TokenType::T_COMMA);
             $this->expressions[] = $parser->ArithmeticFactor();
         }
     <?php } ?>
 <?php } ?>
 
-        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+        $parser->match(TokenType::T_CLOSE_PARENTHESIS);
     }
 
     public function getSql(SqlWalker $sqlWalker): string
